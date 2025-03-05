6e38660e02deb137a1bb7324087ac35b6eed9324
Yann N'DA <yannnda97@gmail.com>
2025-03-05 15:08:42 +0100

    Added Rainbowkit, wagmi, and 2 more to the list of dependencies.

---
+++ front-cryptox/public/components/Provider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { WagmiProvider } from "wagmi";
+import { config } from "../config";
+
+const queryClient = new QueryClient();
+
+export const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
